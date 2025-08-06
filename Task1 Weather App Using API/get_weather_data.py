68438c39fd1d314819b92d75c835592e1206204d
moaskary <mohamadabuabbass@gmail.com>
2025-08-06 16:13:39 +0300

    adding emoji

--- Task1 Weather App Using API/get_weather_data.py
+++ Task1 Weather App Using API/get_weather_data.py
@@ -1,9 +1,9 @@
 import requests
+from apikey import api_key
+
 
 # set the target city
 city = input("Enter city name: ")
-# set the API key from open weather map
-api_key = '204f1cd6ea07ab344596e234f48d0b2d'
 
 # connect to URL 
 url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
@@ -18,9 +18,9 @@ if response.status_code == 200:
     temperature = data['main']['temp']
     humidity = data['main']['humidity']
     wind = data['wind']['speed']
-    print(f"Current weather in {city}: {weather}")
-    print(f"The Temperature is : {temperature}°C")
-    print(f"The humidity is {humidity}")
-    print(f"Wind speed is : {wind}")
+    print(f"🌤️ Current weather in {city}: {weather}")
+    print(f"🌡️ The Temperature is : {temperature}°C")
+    print(f"💧 The humidity is {humidity}")
+    print(f"💨 Wind speed is : {wind}")
 else:
     print(f"Failed to fetch weather data: {response.status_code}")
